646a72b45de369aa7a0f125a51b2690af65ec2e4
nit0002 <nitinsanodiya88@gmail.com>
2024-11-27 23:54:04 +0530

    9th prjt handsContextSseocnd Way

---
+++ 09themeChanger/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState ,useEffect} from 'react'
+import { ThemeProvider } from './Context/ThemeProvider'
+import ThemeBtn from './Component/Themebtn'
+import Card from './Component/Card'
+
+function App() {
+  const [themeMode, setThememode] = useState('light')
+  const darkMode = () => {
+        setThememode("dark")
+  }
+  const lightMode = () => {
+        setThememode("light")
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode) 
+  }, [themeMode])
+  
+
+  return (
+    <ThemeProvider  value={{themeMode , darkMode,lightMode}}>
+      <h1 className='text-center'>Theme Project using Context</h1>
+      
+            <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <ThemeBtn/>
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                        <Card />
+                    </div>
+                </div>
+            </div>
+
+    </ThemeProvider>
+  )
+}
+
+export default App
